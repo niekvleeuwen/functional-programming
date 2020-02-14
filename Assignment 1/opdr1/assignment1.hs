8a7e9a132375c90958a209c0e7ae7b5f7185b749
Niek van Leeuwen <niekvleeuwen@hotmail.nl>
2020-02-14 15:11:47 +0100

    Finished assignment 1

--- Assignment 1/opdr1/assignment1.hs
+++ Assignment 1/opdr1/assignment1.hs
@@ -1,13 +1,13 @@
-faculteit_a :: Int -> Int 
-faculteit_a 0 = 1 
-faculteit_a n = n * faculteit_a(n - 1) 
+faca :: Int -> Int 
+faca 0 = 1 
+faca n = n * faca(n - 1) 
 
-faculteit_b :: Integer -> Integer 
-faculteit_b n | n == 0 = 1 
-              | n /= 0 = n * faculteit_b(n-1) 
+facb :: Integer -> Integer 
+facb n | n == 0 = 1 
+       | n /= 0 = n * facb(n-1) 
 
 main = do 
    putStrLn "De faculteit van 10 is:" 
-   print(faculteit_a 10)
+   print(faca 10)
    putStrLn "De faculteit van 20 is:" 
-   print(faculteit_b 20)
+   print(facb 20)
