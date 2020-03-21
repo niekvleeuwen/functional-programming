6ace82ff89a39b74c0d146819c91ca49b9981db7
Niek van Leeuwen <niekvleeuwen@hotmail.nl>
2020-03-21 14:42:09 +0100

    Added support for negative input

--- Assignment 1/1/assignment1.hs
+++ Assignment 1/1/assignment1.hs
@@ -3,7 +3,8 @@ faca 0 = 1
 faca n = n * faca(n - 1) 
 
 facb :: Int -> Int
-facb n | n == 0 = 1 
+facb n | n < 0 = -1
+       | n == 0 = 1 
        | n /= 0 = n * facb(n-1) 
 
 main = do 
@@ -11,3 +12,4 @@ main = do
    print(faca 10)
    putStrLn "De faculteit van 20 is:" 
    print(facb 20)
+   print(facb (-5))
