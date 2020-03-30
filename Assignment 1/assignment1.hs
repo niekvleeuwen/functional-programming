e1d53b06c0b38e0eb8f099870001cd02a9fc7404
Niek van Leeuwen <niekvleeuwen@hotmail.nl>
2020-03-30 14:18:09 +0200

    Fixes and added comments

--- Assignment 1/assignment1.hs
+++ Assignment 1/assignment1.hs
@@ -10,12 +10,14 @@ Klas : TI2C
 module Opdracht1
     where
 
+-- faculteit met gebruik van pattern matching
 faca :: Int -> Int 
 faca 0 = 1 
 faca n = n * faca(n - 1) 
 
+-- faculteit met gebruik van guards
 facb :: Int -> Int
-facb n | n < 0 = -1
+facb n | n < 0 = error "Faculteit niet mogelijk met min getallen"
        | n == 0 = 1 
        | n /= 0 = n * facb(n-1) 
 
@@ -25,21 +27,23 @@ facb n | n < 0 = -1
 nulpuntena :: Double -> Double -> Double -> [Double]
 nulpuntena a b c = if d < 0 then [0, 0] else [x1, x2]
     where
-        x1 = ((-b) + sqrt(d)) / (2 * a)
-        x2 = ((-b) - sqrt(d)) / (2 * a)
+        x1 = ((-b) - sqrt(d)) / (2 * a)
+        x2 = ((-b) + sqrt(d)) / (2 * a)
         d = b*b - 4*a*c
 
 nulpuntenb :: Double -> Double -> Double -> [Double]
 nulpuntenb a b c | d < 0 = [0, 0] 
                  | d >= 0 = [x1, x2]
                 where
-                    x1 = ((-b) + sqrt(d)) / (2 * a)
-                    x2 = ((-b) - sqrt(d)) / (2 * a)
+                    x1 = ((-b) - sqrt(d)) / (2 * a)
+                    x2 = ((-b) + sqrt(d)) / (2 * a)
                     d = b*b - 4*a*c
 
+-- return alle worpen waarvan de ogen een veelvoud is van vijf
 worpena::[(Int,Int,Int)]
 worpena = [(x,y,z)|x<-[1..6],y<-[1..6],z<-[1..6],((x+y+z) `mod` 5) == 0]
 
+-- return alle worpen waarvan de ogen een veelvoud is van n
 worpenb:: Int -> [(Int,Int,Int)]
 worpenb n = [(x,y,z)|x<-[1..6],y<-[1..6],z<-[1..6],((x+y+z) `mod` n) == 0]
 
@@ -48,8 +52,7 @@ main = do
    print(faca 10)
    putStrLn "De faculteit van 20 is:" 
    print(facb 20)
-   print(facb (-5))
-   print(nulpuntena 1 4 4) -- Result is [-2.0, -2.0]
-   print(nulpuntenb 1 4 4) -- Result is [-2.0, -2.0]
+   print(nulpuntena 1 (-6) (-7)) -- Result is [-1.0, 7.0]
+   print(nulpuntenb 1 (-6) (-7)) -- Result is [-1.0, 7.0]
    print(worpena)
    print(worpenb 7)
