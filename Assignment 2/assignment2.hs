a6dad60c9ae199595856f2c9a16ab0287c5581c2
Niek van Leeuwen <niekvleeuwen@hotmail.nl>
2020-04-01 11:17:18 +0200

    Finished assignment #2

--- Assignment 2/assignment2.hs
+++ Assignment 2/assignment2.hs
@@ -19,42 +19,43 @@ euclid x y
         | otherwise = euclid y (x `mod` y)
 
 -- 1B e * d = 1(mod m)
-egcd::Integer->Integer->[Integer]
-egcd 0 b = [b, 0, 1]
-egcd a b =
-        let [g, s, t] = egcd (b `mod` a) a
-        in [g, t - (b `div` a) * s, s]
+egcd::Integer->Integer->(Integer,Integer,Integer)
+egcd 0 b = (b, 0, 1)
+egcd a b =  
+    let (g, s, t) = egcd (b `mod` a) a
+    in (g, t - (b `div` a) * s, s)
 
-pgcd::Integer->[Integer]->[Integer]
-pgcd m' [a, b, c]
-        | b < 0 = pgcd m' [a, b+m', c]
-        | c < 0 = pgcd m' [a, b, c+m']
-        | otherwise = [a, b, c]
-
+-- maak de uitkomst, als deze negatief is, positief door de modulus er bij op te tellen
+calcD::Integer->Integer->Integer
+calcD e m = if d < 0 then d+m else d
+        where (_,d,_) = egcd e m
+   
 -- 2
-p = 199
-q = 499
-m = p*q
-m' = (p-1)*(q-1)
-e = calcE m' m'- 10
-d = (pgcd m' (egcd e m')) !! 1
 
+-- bereken een getal dat relatief priem is met m'
 calcE::Integer->Integer->Integer
 calcE m' e
         | e > m' = calcE m' (m'-1)
         | (euclid e m') == 1 = e
         | otherwise = calcE m' (e-1)
 
+p = 31
+q = 37
+m = p*q
+m' = (p-1)*(q-1)
+e = calcE m' m'-10
+d = calcD e m'
+
 -- 3
 rsa::(Integer, Integer) -> Integer -> Integer
 rsa (key, m) x = mod (x^key) m       
 
 -- 4
-encryptChar::(Integer, Integer) -> Char -> Integer
-encryptChar (key, m) x = rsa (key, m) (toInteger(ord x))
+rsaencrypt::(Integer, Integer) -> Char -> Integer
+rsaencrypt (key, m) x = rsa (key, m) (toInteger(ord x))
 
-decryptChar::(Integer, Integer) -> Integer -> Char
-decryptChar (key, m) x = chr(fromIntegral(rsa (key, m)  x))
+rsadecrypt::(Integer, Integer) -> Integer -> Char
+rsadecrypt (key, m) x = chr(fromIntegral(rsa (key, m)  x))
 
 {-
     Opdracht 5
@@ -63,4 +64,12 @@ decryptChar (key, m) x = chr(fromIntegral(rsa (key, m)  x))
 -}
 
 main = do 
-   putStrLn "Test" 
+   putStrLn "Bereken de grootste gemenen deler van e en m'"
+   print(euclid e m') 
+   putStrLn "Bereken e·d= 1(mod m):"
+   print(calcD e m')
+   putStrLn "Versleutel de letter 'K':"
+   let key = rsaencrypt (e,m) 'K'
+   print(key)
+   putStrLn "Verkijg de letter met de versleutelde tekst:"
+   print(rsadecrypt (e,m) key)
